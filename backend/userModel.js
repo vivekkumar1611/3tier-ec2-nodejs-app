85ba4ba14cd9b9d0c88ad794594f500d3d74e83e
vivekkumar1611 <k.vivekk1@gmail.com>
2026-04-29 06:59:16 +0530

    Create userModel.js

---
+++ backend/userModel.js
@@ -0,0 +1,33 @@
+const db = require('../config/db');
+
+const User = {
+  create: (name, email, callback) => {
+    db.query(
+      'INSERT INTO users (name, email) VALUES (?, ?)',
+      [name, email],
+      callback
+    );
+  },
+
+  getAll: (callback) => {
+    db.query('SELECT * FROM users', callback);
+  },
+
+  getById: (id, callback) => {
+    db.query('SELECT * FROM users WHERE id = ?', [id], callback);
+  },
+
+  update: (id, name, email, callback) => {
+    db.query(
+      'UPDATE users SET name = ?, email = ? WHERE id = ?',
+      [name, email, id],
+      callback
+    );
+  },
+
+  delete: (id, callback) => {
+    db.query('DELETE FROM users WHERE id = ?', [id], callback);
+  }
+};
+
+module.exports = User;
